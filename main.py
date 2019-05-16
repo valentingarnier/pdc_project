419ac2a639be7472bbba0a644f17f4064b35dd34
Valentin Garnier <valentingarnier@live.fr>
2019-05-16 10:02:01 +0200

    changing result in main

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ for bit in encoder.encode(string):
     if bit == -1:
         array1 = waveformer.create_sinus(constants.FREQUENCY_0_1)
         array2 = waveformer.create_sinus(constants.FREQUENCY_0_2)
-        result = np.concatenate((result, array1, array2)).astype(np.float32)
+        result = np.concatenate((result, (array1 + array2)/2)).astype(np.float32)
     else:
         array1 = waveformer.create_sinus(constants.FREQUENCY_1_1)
         array2 = waveformer.create_sinus(constants.FREQUENCY_1_2)
-        result = np.concatenate((result, array1, array2)).astype(np.float32)
+        result = np.concatenate((result, (array1 + array2)/2)).astype(np.float32)
 
 
 for i in result:
