b850fab1d5dab34ca7b5a10973a57f271c96e48a
Maxime Sierro <greenmaxix@gmail.com>
2019-05-18 14:16:45 +0200

    Amplitude reduite

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ import encoder
 import waveformer
 
 string = "h"
-result = waveformer.create_barker7(2)
+result = []
+result = waveformer.create_barker7(1)
 for bit in encoder.encode(string):
     if bit == -1:
         array1 = waveformer.create_sinus(constants.FREQUENCY_0_1)
         array2 = waveformer.create_sinus(constants.FREQUENCY_0_2)
-        result = np.concatenate((result, (array1 + array2)/2)).astype(np.float32)
+        result = np.concatenate((result, (array1 + array2)/4)).astype(np.float32)
     else:
         array1 = waveformer.create_sinus(constants.FREQUENCY_1_1)
         array2 = waveformer.create_sinus(constants.FREQUENCY_1_2)
-        result = np.concatenate((result, (array1 + array2)/2)).astype(np.float32)
+        result = np.concatenate((result, (array1 + array2)/4)).astype(np.float32)
 
-result = np.concatenate((result, waveformer.create_barker7(2))).astype(np.float32)
+result = np.concatenate((result, waveformer.create_barker7(1))).astype(np.float32)
 
 
 for i in result:
