d24c809473bb8798a1c05691d4ede1413f7da7a3
Valentin Garnier <valentingarnier@live.fr>
2019-05-26 11:39:41 +0200

    encoder besoin du decoder bit -> character

---
+++ decoder.py
@@ -0,0 +1,11 @@
+import numpy as np
+import constants
+
+def decode(frequencies):
+    # decoding the sequence
+    decodedBits = np.empty((constants.SIZE_OF_ENCODED_SEQUENCE, ))
+    for idx, freq in enumerate(frequencies):
+        if freq <= constants.HALF_FREQ:
+            decodedBits[idx] = -1
+        else: decodedBits[idx] = 1
+    return decodedBits
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cutting
 import constants
-from createInput import sizeOfInput
+import decoder
 
 only_data = cutting.cuttingData()
 
@@ -16,27 +16,20 @@ numberOfElemDataSlicedF = len(np.fft.rfft(dataSliced[0]))
 numberOfElemFreqs = len(np.fft.fftfreq(len(dataSliced[0])))
 
 #Initialization of arrays
-dataSlicedF = np.empty((createInput.sizeOfInput, numberOfElemDataSlicedF))
-freqs = np.empty((createInput.sizeOfInput, numberOfElemFreqs))
+dataSlicedF = np.empty((constants.SIZE_OF_ENCODED_SEQUENCE, numberOfElemDataSlicedF))
+freqs = np.empty((constants.SIZE_OF_ENCODED_SEQUENCE, numberOfElemFreqs))
 
 for i, line in enumerate(dataSliced):
     dataSlicedF[i] = np.fft.rfft(line)
     freqs[i] = np.fft.fftfreq(len(line))
 
-
 # Find the peak in the coefficients
-dataSlicedFClean = np.empty((createInput.sizeOfInput, 1))
+dataSlicedFClean = np.empty((constants.SIZE_OF_ENCODED_SEQUENCE, 1))
 for i, line in enumerate(dataSlicedF):
     idx = np.argmax(np.abs(line))
     freq = freqs[i][idx]
     dataSlicedFClean[i] = abs(freq * constants.FREQUENCY_RATE) #freq in hertz
 
+decodedBits = decoder.decode(dataSlicedFClean)
 
-# decoding the sequence
-decodedBits = np.empty((createInput.sizeOfInput, 1))
-for idx, freq in enumerate(dataSlicedFClean):
-    if freq <= constants.HALF_FREQ:
-        decodedBits[idx] = -1
-    else: decodedBits[idx] = 1
-
-print("this is the decoded sequence: ", decodedBits)
+print("this is the decoded sequence: ", decodedBits[: len(decodedBits) - 1])
